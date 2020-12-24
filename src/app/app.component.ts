6994ca76c1348e7b3aed466b6f070093c9d4baad
Marks Machado de Lima Rezende <marksmlr.dev@gmail.com>
2020-12-23 22:54:40 -0300

    Criado projeto algamoney-ui-mmlr, com PrimeNG instalado e configurado para criação de alguns componentes basicos. Trabalhado estilizacao, e adicionado TabView do PrimeNG com aba de formulario e aba de resultado, para testar configuracoes e funcionalidades.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algamoney-ui-mmlr';
+  titleResult: String;
+
+  toUpper = () => this.titleResult = this.title.toUpperCase();
 }
