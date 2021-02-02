4828f8dff237ed4bf11e91c60b41d9ee9c6178fc
Marks Machado de Lima Rezende <marksmlr.dev@gmail.com>
2021-02-02 12:56:43 -0300

    Ajustes de id nos th da tabela.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { style } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,18 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   lancamentos = [
     { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
+    dataPagamento: null, valor: 4.55, pessoa: 'Padaria do José' },
     { tipo: 'RECEITA', descricao: 'Venda de software', dataVencimento: '10/06/2017',
-      dataPagamento: '09/06/2017', valor: 80000.00, pessoa: 'Atacado Brasil' },
+    dataPagamento: '09/06/2017', valor: 80000.00, pessoa: 'Atacado Brasil' },
     { tipo: 'DESPESA', descricao: 'Impostos', dataVencimento: '20/07/2017',
-      dataPagamento: null, valor: 14312.00, pessoa: 'Ministério da Fazenda' },
+    dataPagamento: null, valor: 14312.00, pessoa: 'Ministério da Fazenda' },
     { tipo: 'DESPESA', descricao: 'Mensalidade de escola', dataVencimento: '05/06/2017',
-      dataPagamento: '30/05/2017', valor: 800.00, pessoa: 'Escola Abelha Rainha' },
+    dataPagamento: '30/05/2017', valor: 800.00, pessoa: 'Escola Abelha Rainha' },
     { tipo: 'RECEITA', descricao: 'Venda de carro', dataVencimento: '18/08/2017',
-      dataPagamento: null, valor: 55000.00, pessoa: 'Sebastião Souza' },
+    dataPagamento: null, valor: 55000.00, pessoa: 'Sebastião Souza' },
     { tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '10/07/2017',
-      dataPagamento: '09/07/2017', valor: 1750.00, pessoa: 'Casa Nova Imóveis' },
+    dataPagamento: '09/07/2017', valor: 1750.00, pessoa: 'Casa Nova Imóveis' },
     { tipo: 'DESPESA', descricao: 'Mensalidade musculação', dataVencimento: '13/07/2017',
-      dataPagamento: null, valor: 180.00, pessoa: 'Academia Top' }
+    dataPagamento: null, valor: 180.00, pessoa: 'Academia Top' }
   ];
 }
